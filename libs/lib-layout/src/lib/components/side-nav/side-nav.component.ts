555e7b355b23a5750a8bfc5cb137a011199b7c97
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2025-05-23 23:47:43 +0200

    added material icons

--- libs/lib-layout/src/lib/components/side-nav/side-nav.component.ts
+++ libs/lib-layout/src/lib/components/side-nav/side-nav.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-side-nav',
@@ -7,4 +7,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss',
 })
-export class SideNavComponent {}
+export class SideNavComponent {
+  readonly sideNavMenuItems: SideNavMenuItem[] = [
+    { label: 'Dashboard', icon: 'dashboard', route: '/dashboard' },
+    { label: 'Todo List', icon: 'list', route: '/todolist' },
+    { label: 'Calendar', icon: 'calendar_today', route: '/calendar' },
+    { label: 'Cookbook', icon: 'book', route: '/cookbook' },
+    { label: 'Habits', icon: 'check_circle', route: '/habits' },
+    { label: 'Household', icon: 'home', route: '/household' },
+    { label: 'Inventory', icon: 'inventory', route: '/inventory' },
+  ];
+}
+
+interface SideNavMenuItem {
+  label: string;
+  icon: string;
+  route: string;
+}
