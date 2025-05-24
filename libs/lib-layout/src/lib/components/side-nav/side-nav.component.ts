df105f1d5ef5ef4178b6d81561dea5225b40dc27
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2025-05-24 15:45:24 +0200

    setup simple sidenav menu items

--- libs/lib-layout/src/lib/components/side-nav/side-nav.component.ts
+++ libs/lib-layout/src/lib/components/side-nav/side-nav.component.ts
@@ -1,21 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'lib-side-nav',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss',
 })
 export class SideNavComponent {
   readonly sideNavMenuItems: SideNavMenuItem[] = [
     { label: 'Dashboard', icon: 'dashboard', route: '/dashboard' },
-    { label: 'Todo List', icon: 'list', route: '/todolist' },
+    { label: 'Todo List', icon: 'checklist_rtl', route: '/todolist' },
     { label: 'Calendar', icon: 'calendar_today', route: '/calendar' },
-    { label: 'Cookbook', icon: 'book', route: '/cookbook' },
-    { label: 'Habits', icon: 'check_circle', route: '/habits' },
-    { label: 'Household', icon: 'home', route: '/household' },
-    { label: 'Inventory', icon: 'inventory', route: '/inventory' },
+    { label: 'Cookbook', icon: 'auto_stories', route: '/cookbook' },
+    { label: 'Habits', icon: 'auto_graph', route: '/habits' },
+    { label: 'Household', icon: 'cottage', route: '/household' },
+    { label: 'Inventory', icon: 'inventory_2', route: '/inventory' },
   ];
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     './libs/**/*.{js,ts,jsx,tsx,html}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      sans: ['Outfit', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+    },
   },
   plugins: [require('daisyui')],
   daisyui: {
@@ -13,14 +15,16 @@ module.exports = {
       {
         mytheme: {
           primary: '#3b82f6',
+
           secondary: '#fbbf24',
           accent: '#4ade80',
-          neutral: '#3d4451',
+          neutral: '#ffffff',
           'base-100': '#ffffff',
           info: '#3abff8',
           success: '#36d399',
           warning: '#fbbd23',
           error: '#f87272',
+          dark: '#17171c',
         },
       },
     ],
