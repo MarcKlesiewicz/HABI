3f24c499ae2299cb854ee0472caf4132b3b34d87
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2025-06-01 17:54:02 +0200

    imple swap between checklists

--- libs/feature-airbnb-monitor/src/lib/components/airbnb-checklist/airbnb-checklist.component.ts
+++ libs/feature-airbnb-monitor/src/lib/components/airbnb-checklist/airbnb-checklist.component.ts
@@ -8,6 +8,8 @@ import { Component, signal } from '@angular/core';
   styleUrl: './airbnb-checklist.component.scss',
 })
 export class AirbnbChecklistComponent {
+  isDepartureChecklist = signal<boolean>(true);
+
   departureChecklist = signal<string[]>([
     'Collect and wash all linens and towels',
     'Empty all trash bins and take out the garbage',
@@ -16,5 +18,14 @@ export class AirbnbChecklistComponent {
     'Turn down the thermostat (0) and turn off circulationpump (summer)',
     'Check keys are in keybox',
     'Check for any items that need restocking (toiletries, coffee, etc.)',
+  ]);
+
+  arrivalChecklist = signal<string[]>([
+    'Check that all appliances are functioning properly',
+    'Ensure all lights are working',
+    'Check for any maintenance issues (leaks, broken fixtures, etc.)',
+    'Restock any necessary items (toiletries, coffee, etc.)',
+    'Check that all linens and towels are clean and ready for the next guests',
+    'Ensure the property is clean and ready for the next guests',
   ]);
 }
